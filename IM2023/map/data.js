6be72cecefa9548deaa9f8e95bd5cb94c68d062e
DMP <128150279+DMCDN@users.noreply.github.com>
2024-05-21 15:29:26 +0800

    Add files via upload

---
+++ IM2023/map/data.js
@@ -0,0 +1,389 @@
+const data = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.5309361,
+            25.0439649
+          ]
+        },
+        "properties": {
+          "name": "欣亞數位-台北八德店",
+          "tip": "欣亞數位-台北八德店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.531931,
+            25.045036
+          ]
+        },
+        "properties": {
+          "name": "欣亞數位-台北光華數位一館",
+          "tip": "欣亞數位-台北光華數位一館"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.3138046,
+            24.9904346
+          ]
+        },
+        "properties": {
+          "name": "欣亞數位-桃園NOVA一店",
+          "tip": "欣亞數位-桃園NOVA一店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.2160687,
+            24.955986
+          ]
+        },
+        "properties": {
+          "name": "欣亞數位-中壢明德店",
+          "tip": "欣亞數位-中壢明德店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.664773,
+            24.1506549
+          ]
+        },
+        "properties": {
+          "name": "欣亞數位-台中旗艦店",
+          "tip": "欣亞數位-台中旗艦店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.6651968,
+            24.1517155
+          ]
+        },
+        "properties": {
+          "name": "欣亞數位-台中英才店",
+          "tip": "欣亞數位-台中英才店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.537185,
+            24.0788308
+          ]
+        },
+        "properties": {
+          "name": "欣亞數位-彰化中正店",
+          "tip": "欣亞數位-彰化中正店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.9920796,
+            24.7991725
+          ]
+        },
+        "properties": {
+          "name": "欣亞數位-新竹光復店",
+          "tip": "欣亞數位-新竹光復店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.3089699,
+            22.6374209
+          ]
+        },
+        "properties": {
+          "name": "欣亞數位-高雄建國店",
+          "tip": "欣亞數位-高雄建國店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.329679,
+            22.728007
+          ]
+        },
+        "properties": {
+          "name": "欣亞數位-高雄楠梓店",
+          "tip": "欣亞數位-高雄楠梓店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.3115012,
+            22.6376107
+          ]
+        },
+        "properties": {
+          "name": "欣亞數位-高雄Apple旗艦店",
+          "tip": "欣亞數位-高雄Apple旗艦店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.4887596,
+            22.6724385
+          ]
+        },
+        "properties": {
+          "name": "欣亞數位-屏東中正店",
+          "tip": "欣亞數位-屏東中正店"
+        }
+      }
+    ]
+}
+
+const data1 = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.532274,
+            25.044085
+          ]
+        },
+        "properties": {
+          "name": "原價屋-八德旗艦館",
+          "tip": "原價屋-八德旗艦館"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.5319365,
+            25.0437782
+          ]
+        },
+        "properties": {
+          "name": "原價屋-八德二店",
+          "tip": "原價屋-八德二店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.5319235,
+            25.0449973
+          ]
+        },
+        "properties": {
+          "name": "原價屋-光華門市",
+          "tip": "原價屋-光華門市"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.5313394,
+            25.0446842
+          ]
+        },
+        "properties": {
+          "name": "原價屋-數位三創店",
+          "tip": "原價屋-數位三創店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.531092,
+            25.044391
+          ]
+        },
+        "properties": {
+          "name": "原價屋-組裝工程維修部",
+          "tip": "原價屋-組裝工程維修部"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.5314691,
+            25.0441439
+          ]
+        },
+        "properties": {
+          "name": "原價屋-八德現代店",
+          "tip": "原價屋-八德現代店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.753362,
+            24.7579411
+          ]
+        },
+        "properties": {
+          "name": "原價屋-宜蘭新民店",
+          "tip": "原價屋-宜蘭新民店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.3138774,
+            24.9906344
+          ]
+        },
+        "properties": {
+          "name": "原價屋-桃園復興店",
+          "tip": "原價屋-桃園復興店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.2173275,
+            24.9554781
+          ]
+        },
+        "properties": {
+          "name": "原價屋-中壢中正店",
+          "tip": "原價屋-中壢中正店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            121.0010979,
+            24.7944244
+          ]
+        },
+        "properties": {
+          "name": "原價屋-新竹光復店",
+          "tip": "原價屋-新竹光復店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.6651948,
+            24.151684
+          ]
+        },
+        "properties": {
+          "name": "原價屋-台中英才店",
+          "tip": "原價屋-台中英才店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.5410421,
+            24.0805086
+          ]
+        },
+        "properties": {
+          "name": "原價屋-彰化光復店",
+          "tip": "原價屋-彰化光復店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.4422583,
+            23.4795841
+          ]
+        },
+        "properties": {
+          "name": "原價屋-嘉義林森店",
+          "tip": "原價屋-嘉義林森店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.2119071,
+            22.9942569
+          ]
+        },
+        "properties": {
+          "name": "原價屋-台南北門店",
+          "tip": "原價屋-台南北門店"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.3088711,
+            22.637738
+          ]
+        },
+        "properties": {
+          "name": "原價屋-高雄建國一店",
+          "tip": "原價屋-高雄建國一店"
+        }
+      }
+    ]
+  }
